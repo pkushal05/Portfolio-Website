7849bb8637d2d42d1c2ad787108504ed9278f3b2
pkushal05 <pkushal1052@gmail.com>
2025-05-02 14:19:00 -0400

    Started about me page

---
+++ public/javascripts/homeJs.js
@@ -0,0 +1,9 @@
+
+
+const typed = new Typed(".auto-type-function", {
+    strings: ["Full Stack Developer", "Javascript Enthusiast", "Always learning"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    showCursor: false
+})
