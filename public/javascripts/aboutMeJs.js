6f34f6334395e066a7ce48d254ed288b70b5ffa3
pkushal05 <pkushal1052@gmail.com>
2025-05-02 20:21:30 -0400

    About me page done

---
+++ public/javascripts/aboutMeJs.js
@@ -0,0 +1,9 @@
+
+const typed = new Typed(".auto-type-text", {
+    strings: ["Object Oriented Programming", "Systems Development", "Web Development", "Software Testing", "Software Development"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    showCursor: false,
+    shuffle: true
+});
