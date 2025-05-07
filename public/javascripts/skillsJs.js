60111050b20f6c9ba51e190bdf1aadef46d50c1c
pkushal05 <pkushal1052@gmail.com>
2025-05-07 11:48:56 -0400

    Skills page ready

--- public/javascripts/skillsJs.js
+++ public/javascripts/skillsJs.js
@@ -8,3 +8,39 @@ links.forEach(link => {
         }, 200);
     }
 })
+
+const typedOne = new Typed(".web-development", {
+    strings: ["Web Development"],
+    typeSpeed: 60,
+    showCursor: false
+});
+
+const typedTwo = new Typed(".programming-languages", {
+    strings: ["Programming Languages"],
+    typeSpeed: 60,
+    showCursor: false
+});
+
+const typedThree = new Typed(".back-end", {
+    strings: ["Back-End"],
+    typeSpeed: 60,
+    showCursor: false
+});
+
+const typedFour = new Typed(".tools-ides", {
+    strings: ["Tools &amp; IDEs"],
+    typeSpeed: 60,
+    showCursor: false
+});
+
+const typedFive = new Typed(".software-development", {
+    strings: ["Software Development"],
+    typeSpeed: 60,
+    showCursor: false
+});
+
+const typedSix = new Typed(".soft-skills", {
+    strings: ["Soft Skills"],
+    typeSpeed: 60,
+    showCursor: false
+});
